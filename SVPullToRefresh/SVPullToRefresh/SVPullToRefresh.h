67801b7e39ebdd58428aaf984fb028d818bddcba
Роман Дудка <jonnyro23@gmail.com>
2023-08-06 23:41:32 +0300

    try3

---
+++ SVPullToRefresh/SVPullToRefresh/SVPullToRefresh.h
@@ -0,0 +1,18 @@
+//
+//  SVPullToRefresh.h
+//  SVPullToRefresh
+//
+//  Created by Roma Dudka on 06.08.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SVPullToRefresh.
+FOUNDATION_EXPORT double SVPullToRefreshVersionNumber;
+
+//! Project version string for SVPullToRefresh.
+FOUNDATION_EXPORT const unsigned char SVPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVPullToRefresh/PublicHeader.h>
+
+
